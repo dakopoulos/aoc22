7cd15fa4f957df01f4d63cdaf5dbe7bc87520c3f
Dimitris Dakopoulos <dakopoulos@gmail.com>
2022-12-06 11:31:24 +0000

    Solve day2/part2

--- day2/main.py
+++ day2/main.py
@@ -36,8 +36,41 @@ def read_strategy_file(filename):
                 raise Exception('invalid strategy line: \'%s\'' % line)
     return pnts
 
+def calc_round_points_v2(theirs, outcome):
+    mine = theirs # init
+    if outcome == 1: # loose
+        if theirs == 1:
+            mine = 3
+        elif theirs == 2:
+            mine = 1
+        elif theirs == 3:
+            mine = 2
+    elif outcome == 2: # draw
+        mine = theirs
+    elif outcome == 3: # win
+        if theirs == 1:
+            mine = 2
+        elif theirs == 2:
+            mine = 3
+        elif theirs == 3:
+            mine = 1
+
+    return calc_round_points(theirs, mine)
+
+def read_strategy_file_v2(filename):
+    pnts = 0
+    with open(filename, 'r') as fp:
+        for line in fp:
+            line = line.strip().lower()
+            toks = line.split()
+            if len(toks) == 2:
+                pnts += calc_round_points_v2(convert(toks[0]), convert(toks[1]))
+            else:
+                raise Exception('invalid strategy line: \'%s\'' % line)
+    return pnts
+
 if __name__=='__main__':
     if len(sys.argv) != 2:
         print('usage: python main.py INPUT')
-    pnts = read_strategy_file(sys.argv[1])
+    pnts = read_strategy_file_v2(sys.argv[1])
     print('total points: %d' % pnts)
