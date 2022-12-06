3d2328578dbd577c7f8769c837dfd9436ff4f778
Dimitris Dakopoulos <dakopoulos@gmail.com>
2022-12-06 21:53:01 +0000

    Solve day4/part2

--- day4/main.py
+++ day4/main.py
@@ -2,7 +2,7 @@ import sys
 import re
 import ranges
 
-def read_cleanup_file(filename):
+def read_cleanup_file(filename, full_overlaps_only):
     sum = 0
     with open(filename, 'r') as fp:
         for line in fp:
@@ -12,12 +12,15 @@ def read_cleanup_file(filename):
             r1 = ranges.Range(int(toks[0]), int(toks[1]), include_end=True)
             r2 = ranges.Range(int(toks[2]), int(toks[3]), include_end=True)
             r12 = r1.union(r2)
-            if r12 == r1 or r12 == r2:
+            if (full_overlaps_only == True and (r12 == r1 or r12 == r2))\
+                or (full_overlaps_only == False and r12 != None):
                 sum += 1
     return sum
 
 if __name__=='__main__':
     if len(sys.argv) != 2:
         print('usage: python main.py INPUT')
-    overlaps = read_cleanup_file(sys.argv[1])
-    print('total full overlaps: %d' % overlaps)
+    full_overlaps = read_cleanup_file(sys.argv[1], True)
+    print('total full overlaps: %d' % full_overlaps)
+    partial_overlaps = read_cleanup_file(sys.argv[1], False)
+    print('total partial overlaps: %d' % partial_overlaps)
